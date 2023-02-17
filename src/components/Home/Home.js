e95723d6fa3daeb56ccfa3507ffcf3291c22772f
Sabbir Hossain <sabbirhossainrahad@gmail.com>
2023-02-17 13:52:44 +0600

    initial setup done

---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../Navbar/Navbar';
+
+const Home = () => {
+	return (
+		<div>
+			<h1>This Is Home</h1>
+			<Navbar />
+		</div>
+	);
+};
+
+export default Home;
